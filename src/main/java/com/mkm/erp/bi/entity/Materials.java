7b8bd26298a44a531f097c76742f84a6141f4dd0
Juhee Kim <heeya1004820@naver.com>
2024-10-01 17:56:12 +0900

    Product/Material CRUD

--- src/main/java/com/mkm/erp/bi/entity/Materials.java
+++ src/main/java/com/mkm/erp/bi/entity/Materials.java
@@ -2,30 +2,32 @@ package com.mkm.erp.bi.entity;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Materials {
 
     // Primary Key로 설정된 품목코드
     @Id
-    @Column(name = "id")
+    @Column(name = "id", length = 50, nullable = false)
     private String id;
 
     // 단위 필드, Enum으로 지정
     @Enumerated(EnumType.STRING)
-    @Column(name = "unit")
+    @Column(name = "unit", nullable = false)
     private UnitType unit;
 
     // 품목명 필드
-    @Column(name = "name")
+    @Column(name = "name", length = 50, nullable = false)
     private String name;
 
-    // 수량 필드, Enum으로 지정
+    // 수량 필드
     @Column(name = "quantity")
-    private int quantity;
+    private int quantity = 0;
 
     // 단위에 대한 Enum 정의
     public enum UnitType {
